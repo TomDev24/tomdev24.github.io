07fa4f3a0a2d7f6147bcb7f1101d4956be19ada1
TomDev24 <45002901+TomDev24@users.noreply.github.com>
2019-03-19 12:08:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
 let about = document.getElementById("about")
 
 $(document).ready(()=>{
-  $(".card").on("mouseenter",e=>{
+  if(screen.width < 720){
+    return;
+  }else {
+    $(".card").on("mouseenter",e=>{
 
-    if ($(e.currentTarget).data().name ==="notes" ){
-      about.innerHTML=`<p id="about"><span style="font-weight:bold;">NotesFromTheRoad</span> -
-        адаптивная верстка с использованием FlexBox и jQuery</p>`
-    } else if ($(e.currentTarget).data().name ==="sky" ){
-      about.innerHTML=`<p id="about"><span style="font-weight:bold;">SkyPype</span> -
-        использование технологии React и Redux для иммитации мессенджера</p>`
-    } else if ($(e.currentTarget).data().name ==="blog" ){
-        about.innerHTML=`<p id="about"><span style="font-weight:bold;">SimpleBlog</span> -
-         пример использования фреймворка React</p>`
-    } else {
-      about.innerHTML=`<p id="about"><span style="font-weight:bold;">Ravenous</span> -
-       SPA на React и Yelp API</p>`
-    }
+      if ($(e.currentTarget).data().name ==="notes" ){
+        about.innerHTML=`<p id="about"><span style="font-weight:bold;">NotesFromTheRoad</span> -
+          адаптивная верстка с использованием FlexBox и jQuery</p>`
+      } else if ($(e.currentTarget).data().name ==="sky" ){
+        about.innerHTML=`<p id="about"><span style="font-weight:bold;">SkyPype</span> -
+          использование технологии React и Redux для иммитации мессенджера</p>`
+      } else if ($(e.currentTarget).data().name ==="blog" ){
+          about.innerHTML=`<p id="about"><span style="font-weight:bold;">SimpleBlog</span> -
+           пример использования фреймворка React</p>`
+      } else {
+        about.innerHTML=`<p id="about"><span style="font-weight:bold;">Ravenous</span> -
+         SPA на React и Yelp API</p>`
+      }
 
-        $(".info").show();
+          $(".info").show();
 
-        $(".info").find("p").animate({
-          marginLeft:'5%'
-        },400)
-    })
+          $(".info").find("p").animate({
+            marginLeft:'5%'
+          },400)
+      })
+  }
+
 });
