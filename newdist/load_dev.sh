8eb7b19ca1fe78fcbcb75e7eecba8d0f65c9d2c9
TomDev24 <mail.com67747@gmail.com>
2023-01-22 17:39:41 +0300

    New addition

--- newdist/load_dev.sh
+++ newdist/load_dev.sh
@@ -2,6 +2,22 @@
 
 #wget are not preinstalled on Mac
 
+read -p '(You can run wihout sudo)Make sure that you are running this command in sudo? [y/n] :' ans
+if [ "$ans" != "y" ]; then exit 1 ; fi
+
+sudo apt-get install curl keepass2 vim-runtime vim-gui-common tree
+
+#https://github.com/cli/cli/blob/trunk/docs/install_linux.md
+read -p 'Do you want to install Git Cli? [y/n] :' ans
+if [ "$ans" = "y" ]; then
+	type -p curl >/dev/null || sudo apt install curl -y
+	curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+	&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
+fi
+
 read -p 'would you like to create TOMDEV var which points to website on github? [y/n] :' ans
 if [ "$ans" = "y" ]; then
 	echo 'TOMDEV="https://tomdev24.github.io"' >> $HOME/.bashrc 
@@ -49,11 +65,21 @@ echo '~/.config/Code/User  such a wonderful path by the way'
 echo '-------------'
 
 read -p 'add settings.json and keybindings.json to VSCode config? [y/n] :' ans
-
 if [ "$ans" = "y" ]; then
 	curl https://tomdev24.github.io/newdist/keybindings.json --output ~/.config/Code/User/keybindings.json 
 	curl https://tomdev24.github.io/newdist/settings.json --output ~/.config/Code/User/settings.json 
 fi
+
+read -p 'Do you want to install and configure DropBox ? [y/n] :' ans
+if [ "$ans" = "y" ]; then
+	cd $HOME && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
+	~/.dropbox-dist/dropboxd
+	#wget https://linux.dropbox.com/packages/dropbox.py
+fi
+
+echo; echo;
+echo 'Instal vimuim plugin to firefox'
+echo 'Bookmarks in firefox Telegram/Google/Notion/Github'
 
 #wajig large
 #apt-get remove --purge package
