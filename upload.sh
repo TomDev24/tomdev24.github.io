7ce51a7db689d6cdf928b2117820a1c1d13029f8
TomDev24 <mail.com67747@gmail.com>
2023-12-16 16:36:46 +0300

    new upload

---
+++ upload.sh
@@ -0,0 +1,14 @@
+jekyll build
+mkdir src
+mv ./* ./src
+mv src/_site/* .
+mv src/.nojekyll .
+
+git add -A
+git commit -m "new upload"
+git push
+
+mv ./* src/_site/
+mv src/_site/upload.sh .
+mv src/* .
+/bin/rm -r ./src
